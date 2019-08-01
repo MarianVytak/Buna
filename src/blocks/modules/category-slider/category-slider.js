e2751031e9b8cc867b3b2a58740f5b4aef29c043
MarianVytak <marianvytak@gmail.com>
2019-08-01 06:26:06 +0300

    Tariff: added Category-Slider

---
+++ src/blocks/modules/category-slider/category-slider.js
@@ -0,0 +1,13 @@
+import $ from 'jquery';
+import '../../../../node_modules/slick-carousel/slick/slick.min';
+
+'use strict';
+
+$('.category__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    appendDots: $('.category__slider_dots'),
+    dotsClass: 'category__slider_dots_list'
+});
